a011b6784be102e48724b31bee0964e8713e4c77
brijesh <brijeshkrishnaga@gmail.com>
2022-04-02 11:18:14 +0530

    added license

--- downloader.py
+++ downloader.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2022 Brijesh Krishna
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import time
 from typing import Dict, Union
 import requests
@@ -112,7 +136,7 @@ class downloader:
                     progress.update(
                         task,
                         advance=self.maxSpeed,
-                        description=f"[blue]Speed [green bold]{speedMapper(cur_speed)} [red]Downloading[green] [{downloaded_size}]....",
+                        description=f"[blue]Speed [green bold]{speedMapper(cur_speed)}/s [red]Downloading[green] .... \n      downloaded [{speedMapper(downloaded_size)}]",
                     )
                     if not self.callback == None:
                         self.callback(cur_speed, **self.fn_callback_argc)
